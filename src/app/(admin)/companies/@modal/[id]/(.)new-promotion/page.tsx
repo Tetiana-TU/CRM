3a64b2b900d7eb9b07a89dd08495b2c117cc5d85
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-07-22 09:11:44 +0300

    Next

--- src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
+++ src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
@@ -1,20 +1,20 @@
-'use client';
+// 'use client';
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 import PromotionFormModal from '@/app/components/promotion-form-modal';
-
+import { redirect } from 'next/navigation';
 export interface PageProps {
   params: { id: string };
 }
 
 export default function Page({ params }: PageProps) {
-  const router = useRouter();
+  // const router = useRouter();
   return (
     <PromotionFormModal
       companyId={params.id}
       show={true}
-      onClose={() => router.back()}
+      onClose={() => redirect('/companies')}
     />
   );
 }
